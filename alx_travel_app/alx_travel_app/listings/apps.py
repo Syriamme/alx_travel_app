37bb413fbefd39ba8acb05ccd0c50d4fff7ef090
Syriamme <roridaniel01@gmail.com>
2024-12-09 17:55:35 +0300

    travel app

--- alx_travel_app/alx_travel_app/listings/apps.py
+++ alx_travel_app/alx_travel_app/listings/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ListingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alx_travel_app.listings'  # Update the full path to the app
+    name = 'alx_travel_app.listings'
